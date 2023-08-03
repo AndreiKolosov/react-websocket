278af6844e1ac3f7f118ba11bf45bd14bf633b2e
AndreiKolosov <AndreiKolosov@users.noreply.github.com>
2023-08-03 18:51:51 +0200

    feat: editor state

--- client/src/components/editor/Editor.tsx
+++ client/src/components/editor/Editor.tsx
@@ -1,4 +1,4 @@
-import { type FC, type HTMLProps } from 'react';
+import { type FC, type HTMLProps, useState, useEffect } from 'react';
 // import { isDocumentEvent } from '../../utils';
 import { useWebSocket } from 'react-use-websocket/dist/lib/use-websocket';
 import { WS_URL } from '../../configs/app.config';
@@ -8,29 +8,37 @@ import styles from './Editor.module.css';
 import cn from 'classnames';
 import { WS_EVENTS } from '../../utils/constants';
 
-type TEditorProps = HTMLProps<HTMLElement>
+type TEditorProps = HTMLProps<HTMLElement>;
 
 const Editor: FC<TEditorProps> = ({ className }) => {
+  const [content, setContent] = useState<string>('');
   const { lastJsonMessage, sendJsonMessage } = useWebSocket<TWebSocketMessage>(WS_URL, {
     share: true,
     // filter: isDocumentEvent
   });
 
-  const html =  lastJsonMessage?.payload.editorContent || '';
-
   function handleHtmlChange(e: ContentEditableEvent) {
     sendJsonMessage({
       type: WS_EVENTS.CONTENT_CHANGE,
       payload: {
-        editorContent:  e.target.value
-      }
+        editorContent: e.target.value,
+      },
     });
   }
 
+  useEffect(() => {
+    if (lastJsonMessage?.payload.editorContent) {
+      setContent(lastJsonMessage.payload.editorContent);
+    }
+  }, [lastJsonMessage]);
+
   return (
-    <DefaultEditor value={html} onChange={handleHtmlChange} containerProps={{className: cn(styles.editor, className)}}  />
+    <DefaultEditor
+      value={content}
+      onChange={handleHtmlChange}
+      containerProps={{ className: cn(styles.editor, className) }}
+    />
   );
 };
 
 export { Editor };
-
