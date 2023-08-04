b77c27010be7fddd16545d3340ae4d40575cedd6
AndreiKolosov <AndreiKolosov@users.noreply.github.com>
2023-08-04 13:08:40 +0200

    feat: chat auto scroll

--- client/src/components/chat/Chat.tsx
+++ client/src/components/chat/Chat.tsx
@@ -1,4 +1,4 @@
-import { type FC, type HTMLProps, useState, type FormEvent, useEffect } from 'react';
+import { type FC, type HTMLProps, useState, type FormEvent, useEffect, useRef } from 'react';
 import styles from './Chat.module.css';
 import cn from 'classnames';
 import Typography from '../../ui-kit/typography/Typography';
@@ -8,16 +8,19 @@ import { WS_URL } from '../../configs/app.config';
 import { WS_EVENTS } from '../../utils/constants';
 import { useAppStore } from '../../store/appStore';
 import { isUserEvent } from '../../utils';
+import Avatar from 'react-avatar';
 
 type TChatProps = HTMLProps<HTMLUListElement>;
 
 const Chat: FC<TChatProps> = ({ className }) => {
-  const username = useAppStore(store => store.userName);
+  const username = useAppStore((store) => store.userName);
   const [message, setMessage] = useState<string>('');
-  const [chatData, setChatData] = useState<TChatMessage[]>([])
+  const [chatData, setChatData] = useState<TChatMessage[]>([]);
+  const listRef = useRef<HTMLUListElement>(null)
+  const [prevScrollHeight, setPrevScrollHeight] = useState<number>(0);
   const { lastJsonMessage, sendJsonMessage } = useWebSocket<TWebSocketMessage>(WS_URL, {
     share: true,
-    filter: isUserEvent
+    filter: isUserEvent,
   });
 
   const handleSendMessage = (e: FormEvent) => {
@@ -25,37 +28,58 @@ const Chat: FC<TChatProps> = ({ className }) => {
     sendJsonMessage({
       type: WS_EVENTS.USER_EVENT,
       payload: {
-        chatMessage: {from: username, message},
+        chatMessage: { from: username, message },
       },
     });
-    setMessage('')
-  }
+    setMessage('');
+  };
 
   useEffect(() => {
-    
     if (lastJsonMessage?.payload.chatData) {
       console.log(lastJsonMessage);
       setChatData(lastJsonMessage.payload.chatData);
     }
   }, [lastJsonMessage]);
 
+  useEffect(() => {
+    const list = listRef.current;
+    if(list) {
+      const height = list.scrollHeight
+      list.scrollTo({top: height, behavior: height - prevScrollHeight < 100 ? 'smooth' : 'instant'});
+      setPrevScrollHeight(height)
+      
+    }
+  }, [listRef, chatData, prevScrollHeight])
+
   return (
     <div className={cn(styles.chat, className)}>
       <Typography as="p" variant="caption" color="light" className={styles.chat__title}>
         Chat
       </Typography>
-      <ul className={cn(styles.chat__list, className)}>
-        {chatData && chatData.map((item, index) => (
-          <li className={styles.chat__listItem} key={index}>
-            <Typography as="p" variant="text-s" color="light" fontWeight="medium" className={cn(styles.chat__message, { [styles.chat__message_self]: true })}>
-              {item.message}
-            </Typography>
-          </li>
-        ))}
-        
+      <ul ref={listRef} className={cn(styles.chat__list, className)}>
+        {chatData &&
+          chatData.map((item, index) => (
+            <li className={styles.chat__listItem} key={index}>
+              <Avatar name={item.from} size={'30'} round="20px" />
+              <Typography
+                as="p"
+                variant="text-s"
+                color="light"
+                fontWeight="medium"
+                className={cn(styles.chat__message, { [styles.chat__message_self]: true })}
+              >
+                {item.message}
+              </Typography>
+            </li>
+          ))}
       </ul>
       <form action="submit" className={styles.chat__form} onSubmit={(e) => handleSendMessage(e)}>
-        <input type="text" value={message} className={styles.chat__input} onChange={(e) => setMessage(e.target.value)}/>
+        <input
+          type="text"
+          value={message}
+          className={styles.chat__input}
+          onChange={(e) => setMessage(e.target.value)}
+        />
         <button type="submit" className={styles.chat__formBtn}>
           Send message
         </button>
